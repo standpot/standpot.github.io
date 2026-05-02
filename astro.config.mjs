283603da1f150428cbd036079ee7defaac9d242f
Harry <harrysong.20070102@gmail.com>
2026-05-02 15:23:33 +0900

    Edit Url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,27 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://shravangoswami.com',
-	base: process.env.BASE_PATH || '/astro-scholar',
-	integrations: [
-		mdx(),
-		sitemap()
-	],
-	markdown: {
-		shikiConfig: {
-			themes: {
-				light: 'github-light',
-				dark: 'github-dark',
-			},
-		},
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex]
-	},
-	build: {
-		inlineStylesheets: 'always',
-	},
+  site: "https://standpot.github.io",
+  base: process.env.BASE_PATH || "",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  build: {
+    inlineStylesheets: "always",
+  },
 });
